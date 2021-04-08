6a3b5f0f8e07ac41241ade14435fc4f4ace21dbe
Sohee Kim <sohee@Sohees-MacBook-Air.local>
2021-04-07 20:24:35 -0700

    initial edits

---
+++ script.js
@@ -0,0 +1,48 @@
+var title;
+var executeButton;
+var outputText;
+var inputElement;
+
+var basic = ["white", "black", "gray", "navy", "ivory"]
+var fit = ["jumper", "suit", "loungewear set"]
+var accent = ["dark red", "yellow gold", "forest green", "off white", "royal blue"]
+var acc = ["bucket hat", "silver necklace", "gold chain", "patterned mask"]
+
+
+window.addEventListener('DOMContentLoaded', function() {
+
+title = document.getElementbyId("title");
+executeButton = document.getElementById("fortune");
+outputText = document.getElementById("outputText");
+inputElement = document.getElementById("fortuneInput");
+
+});
+
+executeButton.addEventListener("click", outfitFunction);
+function outfitFunction () {
+
+  var inputName = inputElement.value;
+  console.log(inputName);
+
+
+  var randomBaseIndex = Math.floor(Math.random() * basic.length);
+  var selectedBase = base[randomBaseIndex];
+
+  var randomFitIndex = Math.floor(Math.random() * fit.length);
+  var selectedFit = fit[randomFitIndex];
+
+  var randomAccentIndex = Math.floor(Math.random() * accent.length);
+  var selectedAccent = accent[randomAccentIndex];
+
+  var randomAccIndex = Math.floor(Math.random() * acc.length);
+  var selectedAcc = acc[randomAccIndex];
+
+  var finalOutput = inputName + "'s OOTD: " + selectedBase + selectedFit + ", " + selectedAccent + "socks, and " + selectedAcc;
+
+  outputText.innerHTML= finalOutput;
+
+
+  finalOutput.style.fontSize = "2em";
+  finalOutput.style.backgroundColor = "#FF5733";
+  finalOutput.style.fontFamily = "futura";
+}
