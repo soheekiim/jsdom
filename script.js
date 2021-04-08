26f199839c48ea74e6487c3b5f420d6000782cd0
Sohee Kim <sohee@Sohees-MacBook-Air.local>
2021-04-08 13:57:39 -0700

    final edits

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ var title;
 var executeButton;
 var outputText;
 var inputElement;
+var randomColorIndex;
 
 var basic = ["white", "black", "gray", "navy", "ivory"]
 var fit = ["jumper", "suit", "loungewear set", "overalls"]
 var accent = ["dark red", "yellow gold", "forest green", "off white", "royal blue"]
-var acc = ["bucket hat", "silver necklace", "gold chain", "patterned mask", "pair of Nikes", "pair of Converse", "tote bag"]
+var acc = ["bucket hat", "silver necklace", "gold chain", "patterned mask", "pair of Nikes", "pair of Converse", "tote bag", "beanie"]
+
+var color = ["#f8f9fa", "#f7ede2", "#feeafa", "#edf2fb", "#f2e9e4"]
 
 
 window.addEventListener('DOMContentLoaded', function() {
 
-title = document.getElementbyId("title");
-executeButton = document.getElementById("fortune");
-outputText = document.getElementById("outputText");
-inputElement = document.getElementById("fortuneInput");
+  title = document.getElementById("title");
+  executeButton = document.getElementById("fortune");
+  outputText = document.getElementById("outputText");
+  inputElement = document.getElementById("fortuneInput");
+
+  executeButton.addEventListener("click", outfitFunction);
 
 });
 
-executeButton.addEventListener("click", outfitFunction);
+
 function outfitFunction () {
 
   var inputName = inputElement.value;
@@ -26,7 +31,7 @@ function outfitFunction () {
 
 
   var randomBaseIndex = Math.floor(Math.random() * basic.length);
-  var selectedBase = base[randomBaseIndex];
+  var selectedBase = basic[randomBaseIndex];
 
   var randomFitIndex = Math.floor(Math.random() * fit.length);
   var selectedFit = fit[randomFitIndex];
@@ -37,15 +42,19 @@ function outfitFunction () {
   var randomAccIndex = Math.floor(Math.random() * acc.length);
   var selectedAcc = acc[randomAccIndex];
 
-  var finalOutput = inputName + "'s OOTD: " + selectedBase + selectedFit + ", " + selectedAccent + "socks, and a " + selectedAcc;
+  var finalOutput = inputName + "'s OOTD: " + selectedBase + " " + selectedFit + ", " + selectedAccent + " socks, and a " + selectedAcc;
 
   outputText.innerHTML = finalOutput;
+
+  restyle();
 }
 
 function restyle () {
 
-  finalOutput.style.fontSize = "2em";
-  finalOutput.style.backgroundColor = "#FF5733";
-  finalOutput.style.fontFamily = "futura";
+  randomColorIndex = Math.floor(Math.random() * color.length);
+
+  outputText.style.fontSize = "1em";
+  outputText.style.backgroundColor = color[randomColorIndex];
+  outputText.style.fontFamily = "futura";
 
 }
